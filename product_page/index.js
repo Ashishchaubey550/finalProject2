e9b37d9abcf0b15726632ad734118f49930980c6
Ashish Chaubey <138882318+Ashishchaubey550@users.noreply.github.com>
2024-10-14 11:46:51 +0530

    dropdown

--- product_page/index.js
+++ product_page/index.js
@@ -8,15 +8,6 @@ gsap.from("#product_heading",{
   })
 
 
-var loader = document.querySelector("#preloader");
-var mainContent = document.querySelector("#main");
-
-// Use the window onload event to hide the preloader when all content (including images) is fully loaded
-window.addEventListener('load', function() {
-    loader.style.top = "-100%";
-    mainContent.classList.add('visible'); // Show the main content
-});
-
 
 // var loader = document.querySelector("#preloader");
 // setTimeout(function() {
