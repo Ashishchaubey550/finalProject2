373b37023a695f26aa32b7db2d945e3018df264f
Ashish Chaubey <138882318+Ashishchaubey550@users.noreply.github.com>
2024-12-02 20:03:29 +0530

    contact

--- contactUs/index.js
+++ contactUs/index.js
@@ -5,3 +5,23 @@ gsap.from("#product_heading",{
     duration: 0.5,
     scale: 0.2
   })
+
+let menuList = document.getElementById('nav-part2');
+menuList.style.maxHeight = "0px"; // Start with the menu collapsed
+
+function toggleMenu() {
+  if (menuList.style.maxHeight === "0px") {
+    menuList.style.maxHeight = "300px"; // Expand the menu
+  } else {
+    menuList.style.maxHeight = "0px"; // Collapse the menu
+  }
+}
+// GO to top
+
+document.getElementById('gototop').addEventListener('click', function() {
+  const topNav = document.getElementById('top');
+  topNav.scrollIntoView({
+    behavior: 'smooth',  // Smooth scroll
+    block: 'start'       // Align at the top of the screen
+  });
+});
