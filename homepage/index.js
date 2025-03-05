28d523156dcc4555f04df7663f29ae817ddfa4f2
Ashish Chaubey <138882318+Ashishchaubey550@users.noreply.github.com>
2025-03-05 20:00:30 +0530

    Update index.js

--- homepage/index.js
+++ homepage/index.js
@@ -33,7 +33,7 @@ video.addEventListener('loadeddata', function() {
     setTimeout(function() {
         loader.style.top = "-100%";
         loader.style.transition = "top 1s ease-in";
-
+        
         // Remove preloader from DOM after animation
         loader.addEventListener('transitionend', function() {
             loader.remove();
@@ -57,9 +57,6 @@ video.addEventListener('pause', function() {
     console.log("Video is paused");
     // Optionally, show a play button to the user
 });
-
-// Unmute and play video on user interaction
 document.addEventListener('click', function() {
-    video.muted = false; // Unmute the video
-    video.play(); // Play the video
+    video.play();
 });
