bc8eaf71880bc0eca1faeaec5bdb3179b1908694
Ashish Chaubey <138882318+Ashishchaubey550@users.noreply.github.com>
2025-03-05 13:35:51 +0530

    Final

--- homepage/index.js
+++ homepage/index.js
@@ -25,19 +25,18 @@
 
 
 // Get the elements
+// Get the elements
 var loader = document.querySelector("#preloader");
 var mainContent = document.querySelector("#main");
 var video = document.querySelector("video");
 
 // After 6 seconds, hide the preloader and show the main content
 setTimeout(function() {
-    loader.style.top = "-100%"; // Move the preloader out of view
-    loader.style.transition = "top 1s ease-in"; // Smooth transition
-    mainContent.classList.add('visible'); // Add class to show the main content
+    loader.classList.add('hide'); // Hide the preloader
+    mainContent.classList.add('visible'); // Show the main content
 
     // Restart the video from the beginning
-    video.currentTime = -20;
-    video.play(5000); // Play the video
+    video.currentTime = 0;
+    video.play();
 }, 6500);
-
     
